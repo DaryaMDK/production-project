a61ec72df702863132c76e2ba2a80948aea677ea
DaryaMDK <darya.miloradova@mail.ru>
2023-02-14 14:17:25 +0300

    add scripts lloki raport, update sidebar test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithTranslationt }
-    from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 describe('Sidebar', () => {
     test('Test render sidebar', () => {
-        renderWithTranslationt(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Test toggle sidebar', () => {
-        renderWithTranslationt(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
