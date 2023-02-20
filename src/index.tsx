caea8ef01007fbc8c6ed62e6aa38928778ac1509
DaryaMDK <darya.miloradova@mail.ru>
2023-02-20 14:35:00 +0300

    add entity -counter, redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
@@ -7,12 +8,14 @@ import { ErrorBoundary } from './app/providers/ErrorBoundary';
 import 'app/styles/index.scss';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
